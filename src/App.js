9f13ba9f1126adcdb3b3ed28ad2c1acf6b922a2c
ankit <kumarankit14121999@gmail.com>
2022-10-20 09:15:03 +0530

    image-popup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <>
+    <div className='container'>
+      <div className='image-box'>
+        <input type="radio" id="img-1" name ="image"  checked />
+        <input type="radio" id="img-2" name ="image"  checked/>
+        <input type="radio" id="img-3" name ="image"  checked/>
+        <input type="radio" id="img-4" name ="image"  checked/>
+        <input type="radio" id="img-5" name ="image"  checked/>
+        <div className='image-big'></div>
+      </div>
     </div>
+  </>
   );
 }
 
